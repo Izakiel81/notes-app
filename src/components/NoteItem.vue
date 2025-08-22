886b5ba42ce6b4f0501250477441c2bee3823c6a
Maksym Habinskyi <56722260+Izakiel81@users.noreply.github.com>
2025-08-23 00:40:49 +0200

    first changes

---
+++ src/components/NoteItem.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+</script>	
+<template>
+  <div class="note-item">
+    <p> Hello from note item </p>
+  </div>
+</template>
+
---
+++ src/components/NotesList.vue
@@ -0,0 +1,9 @@
+<script setup>
+import NoteItem from './NoteItem.vue';
+</script>
+<template>
+  <div>
+    <NoteItem/>
+  </div>
+</template>
+<style scoped></style>
