e3a86ecabfba31540ce939aba429c1cecaf40cd1
Maksym Habinskyi <mgabin348@gmail.com>
2025-08-23 20:21:50 +0200

    added form that adds notes

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
-<template>
-  <div id="app">
-    <NotesList/>
-  </div>
-</template>
 <script>
 import NotesList from './components/NotesList.vue';
+import { ref } from 'vue';
 
 export default {
   name: 'App', 
   components: {
     NotesList
+  },
+  setup() {
+
+    const isAddingNote = ref(false);
+
+    function toggleAddingNote() {
+      isAddingNote.value = !isAddingNote.value;
+    }
+
+    return { isAddingNote, toggleAddingNote};
+
   }
 }
 </script>
+
+<template>
+  <div id="app">
+    <button @click="toggleAddingNote" class="add-note">+</button> 
+    <form v-if="isAddingNote">
+	<input/>	
+	<textarea/>	
+	<button >Add Note</button>
+    </form>
+    <NotesList/>
+  </div>
+</template>
 
 <style>
 #app {
@@ -21,4 +40,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
+.add-note {
+  width: 40px;
+  height: 40px;
+  display: block;
+  padding: 10px;
+  margin-bottom: 10px;
+  background-color: #28a745;
+  color: white;
+  border: none;
+  border-radius: 50%; 
+  cursor: pointer;
+}
+
 </style>
--- src/components/NotesList.vue
+++ src/components/NotesList.vue
@@ -7,12 +7,9 @@
 </script>
 
 <template>
-  <div>
-   <button @click="$emit('add-note')" class="add-note">+</button> 
     <div v-for="note in notes" :key="note.id"> 
       <NoteItem :note="note" />
     </div> 
-  </div>
 </template>
 
 <style scoped>
@@ -23,17 +20,5 @@
   border-radius: 5px;
   background-color: #f9f9f9;
   padding: 10px;
-}
-.add-note {
-  width: 40px;
-  height: 40px;
-  display: block;
-  padding: 10px;
-  margin-bottom: 10px;
-  background-color: #28a745;
-  color: white;
-  border: none;
-  border-radius: 50%; 
-  cursor: pointer;
 }
 </style>
