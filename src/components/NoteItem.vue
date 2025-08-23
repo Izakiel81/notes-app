94338dc9b57e1cf3bc8669310e34dc0efdddb55f
Maksym Habinskyi <mgabin348@gmail.com>
2025-08-23 19:01:26 +0200

    sample notes reading

--- src/components/NoteItem.vue
+++ src/components/NoteItem.vue
@@ -1,8 +1,23 @@
-<script lang="ts" setup>
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+  note: {
+    id: Number,
+    title: String,
+    content: String,
+  } 
+});
 </script>	
 <template>
   <div class="note-item">
-    <p> Hello from note item </p>
+    <h3>{{ note.title }}</h3>
+    <p>{{ note.content }}</p>
   </div>
 </template>
 
+<style scoped>
+.note-item {
+  padding: 10px;
+  border-bottom: 1px solid #eee; 
+}
+</style>
--- src/components/NotesList.vue
+++ src/components/NotesList.vue
@@ -1,9 +1,39 @@
-<script setup>
-import NoteItem from './NoteItem.vue';
+<script setup >
+  import NoteItem from './NoteItem.vue';
+  import { ref } from 'vue';
+  
+  const notes = ref([{ id: 0, title: 'Sample Note', content: 'This is a sample note.' }]);
+
 </script>
+
 <template>
   <div>
-    <NoteItem/>
+   <button @click="$emit('add-note')" class="add-note">+</button> 
+    <div v-for="note in notes" :key="note.id"> 
+      <NoteItem :note="note" />
+    </div> 
   </div>
 </template>
-<style scoped></style>
+
+<style scoped>
+.note-list {
+  max-width: 600px;
+  margin: 20px auto;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f9f9f9;
+  padding: 10px;
+}
+.add-note {
+  width: 40px;
+  height: 40px;
+  display: block;
+  padding: 10px;
+  margin-bottom: 10px;
+  background-color: #28a745;
+  color: white;
+  border: none;
+  border-radius: 50%; 
+  cursor: pointer;
+}
+</style>
