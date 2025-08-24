2812e099818365635cec8ca25b64ba90ca1e971e
Maksym Habinskyi <mgabin348@gmail.com>
2025-08-24 06:16:31 +0200

    minor changes

--- src/components/NoteAddDialog.vue
+++ src/components/NoteAddDialog.vue
@@ -1,25 +1,32 @@
 <script>
-import { ref, defineEmits } from 'vue';
-const emit = defineEmits(['add-note']);
-const newNoteTitle = ref('');
-const newNoteContent = ref('');
-function addNote() {
-  if (!newNoteTitle.value.trim() || !newNoteContent.value.trim()) return;
+  
+  import { ref, defineEmits } from 'vue';
+  
+  const emit = defineEmits(['add-note']);
+  const newNoteTitle = ref('');
+  const newNoteContent = ref('');
+  
+  function addNote() {
+    if (!newNoteTitle.value.trim() || !newNoteContent.value.trim()) return;
 
-  emit('add-note', {
-    title:newNoteTitle.value,
-    content:newNoteContent.value
-  });
-  newNoteTitle.value = '';
-  newNoteContent.value = ''; 
-}
+    emit('add-note', {
+      title:newNoteTitle.value,
+      content:newNoteContent.value
+    });
+    newNoteTitle.value = '';
+    newNoteContent.value = ''; 
+  }
+
 </script>
 <template>
-	<form> 
-	<input />	
-	<textarea />	
-	<button >Add Note</button>
-    </form>
+  <form @submit.prevent="addNote"> 
+   <input v-model="newNoteTitle" />	
+   <textarea v-model="newNoteContent" />	
+
+   <button>Add Note</button>
+   <button @click="cancel">Cancel</button>
+  </form>
 </template>
 <style scoped>
+
 </style>
